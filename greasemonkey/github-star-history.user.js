f55be43142ab6416470b1af836a7f09fb09ecf41
Adam Lui 刘展鹏 <adamlui@protonmail.com>
2024-10-17 14:47:35 -0700

    Bumped `@version` to `2024.10.17.15`

--- greasemonkey/github-star-history.user.js
+++ greasemonkey/github-star-history.user.js
@@ -13,7 +13,7 @@
 // @description:zh-TW   將明星曆史圖表添加到 GitHub 存儲庫的側邊欄
 // @author              Adam Lui
 // @namespace           https://github.com/adamlui
-// @version             2024.10.17.14
+// @version             2024.10.17.15
 // @license             MIT
 // @icon                https://github.githubassets.com/favicons/favicon.png
 // @compatible          chrome
