6b05426ec5e12aaacfc99de35f4c4e4f44c94936
adamlui@protonmail.com <adamlui@protonmail.com>
2024-11-11 13:46:45 -0800

    Added eslint-plugin-yml as dev dependency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import json from '@eslint/json'
+import markdown from '@eslint/markdown'
+import eslintPluginYml from 'eslint-plugin-yml'
 
 export default [
     {
@@ -22,5 +24,14 @@ export default [
         }
     },
     { files: ['**/*.mjs'], languageOptions: { sourceType: 'module' }},
-    { files: ['**/*.json'], ignores: ['**/package-lock.json'], language: 'json/json', ...json.configs.recommended }
+    { files: ['**/*.json'], ignores: ['**/package-lock.json'], language: 'json/json', ...json.configs.recommended },
+    {
+        files: ['**/*.md'], language: 'markdown/commonmark', plugins: { markdown },
+        rules: {
+            ...markdown.configs.recommended[0].rules,
+            'markdown/heading-increment': 'off', // allow headings to skip levels
+            'markdown/fenced-code-language': 'off' // allow code blocks w/ no language specified
+        }
+    },
+    { files: ['**/*.yaml, **/*.yml'], ...eslintPluginYml.configs['flat/standard'][1] }
 ]
